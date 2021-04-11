c6d34204532a1e7d421b19526099d6783cc4bd51
Alex BAIET <alex.baiet3@gmail.com>
2021-04-11 20:39:43 +0200

    Initial commit

---
+++ Pokedex/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Pokédex</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+  <p>
+    Bienvenue sur le Pokédex !
+    Le site est encore vide pour le moment.
+  </p>
+</body>
+</html>
